2ecf57b786ce5be1ddb0264bcc333e9e197d5f60
sonnc <sonnc@lifull-tech.vn>
2024-02-25 23:03:12 +0700

    thiep mung

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Ngọc Anh';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 8/3';
+const titleCard = 'Tặng người bạn';
+const contentCard = 'Chúc bạn của tớ 8/3 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với cậu trong hôm nay và cả những ngày sau';
+
+const giftImage = 'ngoc_anh.jpeg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
